323a5b1cda76c8e14774c5aec43bac01aaa7350f
Muhammadrasul Gulomjonov <mgulomjonov151@gmail.com>
2026-09-08 23:16:24 +0500

    Meta Pixelni HTML head ga rasmiy snippet sifatida qo'yish

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,31 +34,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        {FB && (
+          <>
+            <script
+              dangerouslySetInnerHTML={{
+                __html:
+                  `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?` +
+                  `n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;` +
+                  `n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;` +
+                  `t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,` +
+                  `document,'script','https://connect.facebook.net/en_US/fbevents.js');` +
+                  `fbq('init', '${FB}');fbq('track', 'PageView');`,
+              }}
+            />
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${FB}&ev=PageView&noscript=1"/>`,
+              }}
+            />
+          </>
+        )}
       </head>
       <body>
         {children}
-
-        {FB && (
-          <>
-            <Script id="fb-pixel" strategy="afterInteractive">
-              {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-              n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-              document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init','${FB}');fbq('track','PageView');`}
-            </Script>
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${FB}&ev=PageView&noscript=1`}
-                alt=""
-              />
-            </noscript>
-          </>
-        )}
 
         {YM && (
           <Script id="ym" strategy="afterInteractive">
